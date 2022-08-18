1fb2b6a5aac9be0ca5471a9b705a1446ba80ca02
Rafał <48444061+TheRafikiPL@users.noreply.github.com>
2022-08-18 16:18:58 +0200

    Initial commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ShopProductsList;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
+
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace ShopProductsList;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
